602bb425d3fe364bcc6b74ef0920aee9f525759f
Tiffany <farrisluke8@gmail.com>
2024-08-22 01:42:12 -0600

    autostart fart

--- config.h
+++ config.h
@@ -30,12 +30,24 @@ const char *modes_labels[] = {
 /* logging */
 static int log_level = WLR_ERROR;
 
+/* Autostart */
+static const char *const autostart[] = {
+    "waybar", NULL,
+    "dunst", NULL,
+    "swaybg", "-i", "/home/tiffany/Pictures/Wallpapers/cool_madeline_wallpaper.png", NULL,
+    "foot", NULL,
+    "firefox", NULL,
+        //"wbg", "/path/to/your/image", NULL,
+        NULL /* terminate */
+};
+
+
 /* NOTE: ALWAYS keep a rule declared even if you don't use rules (e.g leave at least one example) */
 static const Rule rules[] = {
 	/* app_id             title       tags mask     isfloating   monitor */
 	/* examples: */
 	{ "Gimp_EXAMPLE",     NULL,       0,            1,           -1 }, /* Start on currently visible tags floating, not tiled */
-	{ "firefox_EXAMPLE",  NULL,       1 << 8,       0,           -1 }, /* Start on ONLY tag "9" */
+	{ "Mozilla Firefox",  NULL,       1 << 2,       0,           -1 }, /* Start on ONLY tag "3" */
 };
 
 /* layout(s) */
